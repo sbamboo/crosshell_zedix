c5e3eec478d79db2049425a356fe1dcbc1353847
StalkedBamboo <simon.claesson8@gmail.com>
2022-12-21 21:36:05 +0100

    Update packagehand.py

--- packages/cmdlets/crosshell_packagehand/packagehand.py
+++ packages/cmdlets/crosshell_packagehand/packagehand.py
@@ -1,1 +1,18 @@
 import argparse
+import requests
+
+cparser = argparse.ArgumentParser(prog="Packagehand")
+# Install
+cparser.add_argument('--install','--add','--a', dest="install", action='store_true', help="Install switch")
+# Package (Comsume al remaining arguments)
+cparser.add_argument('-package','-p', nargs='*', dest="package", help="The package id")
+# Create main arguments object
+argus = cparser.parse_args(argv)
+
+
+# Get repo
+repository_url = ""
+repository = content = (requests.get(repository_url)).text
+
+# Install
+if argus.install == True:
