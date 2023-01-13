30baf5a525a67447857b7d3e45217181ed826dff
StalkedBamboo <85576459+simonkalmiclaesson@users.noreply.github.com>
2023-01-13 10:25:01 +0100

    update install req

--- .install/install.ps1
+++ .install/install.ps1
@@ -1,5 +1,5 @@
 py -m pip install pyyaml
-py -m pip insyall tqdm
+py -m pip install tqdm
 py -m pip install requests
-py -m pip install promt_toolkit
+py -m pip install prompt_toolkit
 py -m pip install pygments
