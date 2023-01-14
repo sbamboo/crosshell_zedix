781cb6fcf3136b07eb4706a40020f53c44ddf93d
StalkedBamboo <simon.claesson8@gmail.com>
2023-01-14 22:21:08 +0100

    add portablemc wrapper

---
+++ packages/cmdlets/simonkalmiclaesson.portablemcWrapper.latest/portablemc.py
@@ -0,0 +1,13 @@
+# [Imports]
+hasinstalledState = f"{CSScriptRoot}{os.sep}hasinstalled.empty"
+if not os.path.exists(hasinstalledState):
+    print("Installing portablemc...")
+    os.system("python3 -m pip install --user portablemc")
+    os.system("python3 -m pip install --user portablemc-fabric")
+    os.system("python3 -m pip install --user portablemc-forge")
+    os.system("python3 -m pip install --user portablemc-quilt")
+    outFile("1",hasinstalledState)
+
+# [Run portable mc]
+commandString = "python3 -m portablemc " + (' '.join(argv)).strip(" ")
+os.system(commandString)
