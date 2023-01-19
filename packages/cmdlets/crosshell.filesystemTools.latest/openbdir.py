e88209670a7d1c77bc5b30a7f8253d00a2c72e32
StalkedBamboo <85576459+simonkalmiclaesson@users.noreply.github.com>
2023-01-19 22:54:36 +0100

    update
    ~ Make openbdir and opendir cmdlets crossplatform (on sertain platforms)

--- packages/cmdlets/crosshell.filesystemTools.latest/openbdir.py
+++ packages/cmdlets/crosshell.filesystemTools.latest/openbdir.py
@@ -1,1 +1,9 @@
-os.system(f"explorer {csbasedir}")
+try: import distro
+except:
+    os.system("python3 -m pip install distro")
+
+if IsWindows(): os.system(f"explorer {csbasedir}")
+elif IsMacOS(): os.system(f"open {csbasedir}")
+elif IsLinux():
+    #Rassberry pi
+    if distro.id() == "raspbian": os.system(f"pcmanfm {csbasedir}")
--- packages/cmdlets/crosshell.filesystemTools.latest/opendir.py
+++ packages/cmdlets/crosshell.filesystemTools.latest/opendir.py
@@ -1,1 +1,9 @@
+try: import distro
+except:
+    os.system("python3 -m pip install distro")
+
 if IsWindows(): os.system("explorer .")
+elif IsMacOS(): os.system("open .")
+elif IsLinux():
+    #Rassberry pi
+    if distro.id() == "raspbian": os.system("pcmanfm .")
