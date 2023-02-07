8d9f7c35ca60f6d82c57c302b7cc2d1b3f920b9f
StalkedBamboo <85576459+simonkalmiclaesson@users.noreply.github.com>
2023-02-07 09:10:45 +0100

    Update actionHandler.py
    + Fix error if actions is not used

--- assets/actionHandler.py
+++ assets/actionHandler.py
@@ -1,20 +1,21 @@
 def cs_RunActions(actions_string,basedir=None):
-    actions_string = actions_string.replace(" ","")
-    actions = actions_string.split(";")
+    if actions_string:
+        actions_string = actions_string.replace(" ","")
+        actions = actions_string.split(";")
 
-    actions_list = ["list: Lists out actions.","CdBdir: Cd's to basedir. (Crosshell's install directory)"]
+        actions_list = ["list: Lists out actions.","CdBdir: Cd's to basedir. (Crosshell's install directory)"]
 
-    # Run Actions
-    for action in actions:
-        # List
-        if action.lower() == "list":
-            print("\033[94mActions:\033[0m")
-            for actions_list_item in actions_list:
-                print(actions_list_item)
-            exit()
-        # CdBasedir
-        if action.lower() == "cdbdir":
-            if basedir != None:
-                print( basedir )
-            exit()
+        # Run Actions
+        for action in actions:
+            # List
+            if action.lower() == "list":
+                print("\033[94mActions:\033[0m")
+                for actions_list_item in actions_list:
+                    print(actions_list_item)
+                exit()
+            # CdBasedir
+            if action.lower() == "cdbdir":
+                if basedir != None:
+                    print( basedir )
+                exit()
 
