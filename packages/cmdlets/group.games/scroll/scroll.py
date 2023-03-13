ba2d2b6f3d583f95ca0cf89f29f84fb2114fb079
StalkedBamboo <85576459+simonkalmiclaesson@users.noreply.github.com>
2023-03-13 16:08:51 +0100

    Delete packages/cmdlets/group.games/scroll directory

--- packages/cmdlets/group.games/scroll/scroll.py
+++
@@ -1,5 +0,0 @@
-# [Imports]
-from assets.lib.drawlib.SimpleSpriteRenderer import *
-from assets.lib.drawlib.linedraw import *
-from assets.lib.drawlib.asset import *
-from assets.lib.drawlib.tui import *
