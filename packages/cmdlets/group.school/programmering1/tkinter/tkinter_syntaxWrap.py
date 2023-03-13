7751c161bcd5a34fcff60e70cb6df2be81c0cc39
StalkedBamboo <85576459+simonkalmiclaesson@users.noreply.github.com>
2023-03-13 16:09:14 +0100

    Delete packages/cmdlets/group.school directory

--- packages/cmdlets/group.school/programmering1/tkinter/tkinter_syntaxWrap.py
+++
@@ -1,2 +0,0 @@
-import tkinter as tk
-print( dir(tk.Tk) )
--- packages/cmdlets/group.school/programmering1/upg/upgrun.py
+++
@@ -1,8 +0,0 @@
-# [Imports]
-import assets.lib.importa as im
-tasks = im.fromPath(f"{CSScriptRoot}\.Tasks.py")
-
-# Code:
-
-StringToRun = argv[0]
-exec(f"tasks.{StringToRun}")
